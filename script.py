bb9f721d01c966011ca4ffd3a88798c86f6d2567
Brandon Sullivan <sullivanb13@wit.edu>
2022-12-06 13:44:46 -0500

    Creating the Stacks tasks complete

--- script.py
+++ script.py
@@ -3,6 +3,15 @@ from stack import Stack
 print("\nLet's play Towers of Hanoi!!")
 
 # Create the Stacks
+stacks = []
+
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
 
 # Set up the Game
 
